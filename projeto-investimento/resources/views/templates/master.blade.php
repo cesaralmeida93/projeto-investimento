7680f00e80171d0459002b8b88590e868b3b51ed
cesar <ocesaralmeida@gmail.com>
2019-05-22 09:59:56 -0300

    starting modeling template view

---
+++ projeto-investimento/resources/views/templates/master.blade.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Investindo</title>
+		@yield('css-view')
+		<link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+	</head>
+	
+	<body>
+		@yield('conteudo-view')
+		@yield('js-view')
+	</body>
+</html>
