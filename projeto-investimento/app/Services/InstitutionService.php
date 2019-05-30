c7825519d73cba728b63b7c397626217312b34d7
cesar <ocesaralmeida@gmail.com>
2019-05-30 14:08:20 -0300

    Persistencia de dados com L5 Repository

---
+++ projeto-investimento/app/Services/InstitutionService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\InstitutionRepository;
+use App\Validators\InstitutionValidator;
+use Prettus\Validator\Contracts\ValidatorInterface;
+
+
+
+class InstitutionService
+{
+	private $repository;
+	private $validator;
+
+	public __construct(InstitutionRepository $repository, InstitutionValidator $validator)
+	{
+		$this->repository = $repository;
+		$this->validator = $validator;
+	}
+
+	public function store(array $data)
+	{
+		try
+		{
+			$this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_CREATE);
+			$institution = $this->repository->create($data);
+
+
+
+			return [
+				'success'  => true,
+				'messages' => "Instituição cadastrada",
+				'data'	   => $institution, 
+			];
+		}		
+		catch(Exception $e)
+		{
+			switch (get_class($e))
+			{
+				case QueryException::class     	   : return ['success' => false, 'messages' => $e->getMessage()];  
+				case ValidatorException::class     : return ['success' => false, 'messages' => $e->getMessageBag()]; 
+				case Exception::class     		   : return ['success' => false, 'messages' => $e->getMessage()]; 
+				default                            : return ['success' => false, 'messages' => $e->getMessage()]; 
+			}
+
+
+		}
+	}
+}
--- projeto-investimento/app/Validators/InstitutionValidator.php
+++ projeto-investimento/app/Validators/InstitutionValidator.php
@@ -18,7 +18,9 @@ class InstitutionValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
+        ValidatorInterface::RULE_CREATE => [
+        	'name' => 'required',
+        ],
         ValidatorInterface::RULE_UPDATE => [],
     ];
 }
